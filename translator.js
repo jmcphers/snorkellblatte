59377e959b0a91122c9415cf87c09bd9e824c569
Jonathan McPherson <jonathan@rstudio.com>
2022-01-16 22:20:54 -0800

    now with glyphs

--- translator.js
+++ translator.js
@@ -4,17 +4,23 @@ const snorkell = "UTMJIZPXOBQKDSAWGCXNEFRLHVutmjizpxobqkdsawgcxnefrlhv";
 let translate = function() {
     let english = document.getElementById("english");
     let spoken = document.getElementById("spoken");
+    let written = document.getElementById("written");
     let before = english.value;
     let after = "";
+    let glyphs = "";
     for (let i = 0; i <= before.length; i++) {
         let ch = before.charAt(i);
         let idx = alpha.indexOf(ch);
         if (idx > 0) {
             after += snorkell.charAt(idx);
+            glyphs += "<img src='img/snorkellblatte-" + ch.toLowerCase() + ".png'/>"
         } else {
+            // TODO: fix XSS
+            glyphs += ch;
             after += ch;
         }
     }
+    written.innerHTML = glyphs;
     spoken.innerText = after;
 }
 
